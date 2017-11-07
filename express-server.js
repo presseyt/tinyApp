788a88236a955b5045537913dfc2f962c63572d6
Timothy Pressey <timothy.pressey@gmail.com>
2017-11-07 22:37:01 +0000

    added body-parser to package.json, read post requests using body-parser, added a generateRandomString function

--- express-server.js
+++ express-server.js
@@ -1,17 +1,43 @@
 const express = require('express');
 const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
 var PORT = process.env.PORT || 8080;
+
 
 var urlDatabase = {
   'asdf': 'http://google.ca',
   'sdfg': 'http://www.lighthouselabs.ca'
 };
 
+function generateRandomString(){
+  let output = "";
+  for(let i = 0; i < 6; i++){
+    let rnd = Math.floor(Math.random() * 36);
+    rnd = rnd < 10 ? rnd + 49 : rnd + 87;
+    output += String.fromCharCode(rnd);
+  }
+  return output;
+}
+
+console.log(generateRandomString(), generateRandomString(), generateRandomString());
+
 app.set("view engine", "ejs");
+
+app.get("/urls/new", (req,res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 app.get("/urls", (request,response) => {
   let templateVars = {urls: urlDatabase};
-  response.render("urls_new", templateVars);
+  response.render("urls_index", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
