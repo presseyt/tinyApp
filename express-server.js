2c040a0b8bb0df84f99b7454524187e158c68185
Timothy Pressey <timothy.pressey@gmail.com>
2017-11-07 21:57:43 +0000

    added ejs templates for /urls and /urls/:short and handled get requests for each

---
+++ express-server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+var PORT = process.env.PORT || 8080;
+
+var urlDatabase = {
+  'asdf': 'http://google.ca',
+  'sdfg': 'http://www.lighthouselabs.ca'
+};
+
+app.set("view engine", "ejs");
+
+app.get("/urls", (request,response) => {
+  let templateVars = {urls: urlDatabase};
+  response.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id, urls: urlDatabase};
+  res.render("urls_show", templateVars);
+});
+
+console.log(`Listening on port ${PORT}!`);
+app.listen(PORT);
